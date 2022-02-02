73c6d6af7c7d486bffd1bf909a8955febd929576
Tiago Sousa <tiagocspsousa@gmail.com>
2022-02-02 01:45:27 +0000

    Added changes to app and model

--- app.py
+++ app.py
@@ -109,31 +109,10 @@ pipeline = joblib.load('pipeline.pickle')
 ########################################
 # Unpickle the previously-trained model
 
-def check_request_id(request):
-    """
-        Validates that our request is well formatted
-        
-        Returns:
-        - assertion value: True if request is ok, False otherwise
-        - error message: empty if request is ok, False otherwise
-    """
-    
-    if "admission_id" not in request:
-        error_description = "Field `admission_id` missing from request: {}".format(request)
-        return False, error_description
-    
-    return True, ""
-
 def check_valid_column(observation):
-    """
-        Validates that our observation only has valid columns
-        
-        Returns:
-        - assertion value: True if all provided columns are valid, False otherwise
-        - error message: empty if all provided columns are valid, False otherwise
-    """
     
-    valid_columns = {'patient_id', 
+    valid_columns = {'admission_id',
+                     'patient_id', 
                      'race', 
                      'gender', 
                      'age', 
@@ -142,7 +121,8 @@ def check_valid_column(observation):
                      'discharge_disposition_code',
                      'admission_source_code', 
                      'time_in_hospital', 
-                     'payer_code','medical_specialty',
+                     'payer_code',
+                     'medical_specialty',
                      'has_prosthesis',
                      'complete_vaccination_status',
                      'num_lab_procedures', 
@@ -182,7 +162,63 @@ def check_valid_column(observation):
 
     return True, "",""
 
-def check_categorical_values(observation):
+def check_column_types(observation):
+
+    valid_column_types = {
+                     'admission_id':[1,""],
+                     'patient_id':[1,""], 
+                     'race':["" , None], 
+                     'gender':["" , None], 
+                     'age':["" , None], 
+                     'weight':["" , None],
+                     'admission_type_code':[1.0,None], 
+                     'discharge_disposition_code':[1.0,None],
+                     'admission_source_code':[1,"",None], 
+                     'time_in_hospital':[1,"",None], 
+                     'payer_code':["",None],
+                     'medical_specialty':["",None],
+                     'has_prosthesis':[True,"",None],
+                     'complete_vaccination_status':["",None],
+                     'num_lab_procedures':[1.0,None], 
+                     'num_procedures':[1,"",None],
+                     'num_medications':[1.0,None], 
+                     'number_outpatient':[1,"",None], 
+                     'number_emergency':[1,"",None], 
+                     'number_inpatient':[1,"",None], 
+                     'diag_1':["",None], 
+                     'diag_2':["",None], 
+                     'diag_3':["",None], 
+                     'number_diagnoses':[1,"",None], 
+                     'blood_type':["",None], 
+                     'hemoglobin_level':[1.0,None], 
+                     'blood_transfusion':[True,"",None], 
+                     'max_glu_serum':["",None], 
+                     'A1Cresult':["",None], 
+                     'diuretics':["",None], 
+                     'insulin':["",None], 
+                     'change':["",None], 
+                     'diabetesMed':["",None], 
+    }
+    for key, valid_columns in valid_column_types.items():
+        if key in observation:
+            value = observation[key]
+            if type(value).__name__ not in [type(x).__name__ for x in valid_columns]:
+                error = "Invalid datatype provided for '{}': '{}'. Allowed datatypes are: {}".format(
+                    key, type(value).__name__, ",".join(["'{}'".format(type(v).__name__) for v in valid_columns]))
+                return False, error
+            
+            if key in ['admission_id','patient_id','admission_source_code','time_in_hospital','num_procedures','number_outpatient','number_emergency','number_inpatient','number_diagnoses'] and isinstance(value,str):
+                try:
+                    observation[key] = int(value)
+                except:
+                    error = "Invalid datatype provided for '{}': '{}'. Transformation to '{}' is not possible".format( key, type(value).__name__,(type(1).__name__))
+                    return False, error
+                    
+        
+
+    return True, ""
+
+def check_column_values(observation):
     """
         Validates that all categorical fields are in the observation and values are valid
         
@@ -200,14 +236,24 @@ def check_categorical_values(observation):
         if key in observation:
             value = observation[key]
             if value not in valid_categories:
-                error = "Invalid value provided for {}: {}. Allowed values are: {}".format(
-                    key, value, ",".join(["'{}'".format(v) for v in valid_categories]))
+                error = "Invalid value provided for {}: {}. Allowed values are: {}".format( key, value, ",".join(["'{}'".format(v) for v in valid_categories]))
                 return False, error
         #else:
         #    error = "Categorical field {} missing"
         #    return False, error
 
     return True, ""
+
+
+def get_model_prediction(pred_value):
+    readmitted = ""
+        
+    if pred_value == 1:
+        readmitted = "Yes"
+    elif pred_value == 0 :
+        readmitted = "No"
+        
+    return readmitted
 
 # End model un-pickling
 ########################################
@@ -224,56 +270,45 @@ def predict():
     warning = False
     warning_description = ""
     
-    obs_dict = request.get_json()
-
-    request_ok, error_description = check_request_id(obs_dict)
-    if not request_ok:
-        response = {'id': None,'error': error_description}
-        r = Request(request=obs_dict, response=response, endpoint='predict', status='error')
-        r.save()
-        return response
-
-    _id = obs_dict['admission_id']
-   
-    
-    observation = obs_dict
+    observation = request.get_json()
     
     columns_ok, error_description, error_type = check_valid_column(observation)
     if not columns_ok and error_type=='failure':
-        response = {'id': _id,'error': error_description}
-        r = Request(request=obs_dict, response=response, endpoint='predict', status='error')
+        response = {'admission_id': obs_dict['admission_id'],'error': error_description}
+        r = Request(request=observation, response=response, endpoint='predict', status='error')
         r.save()
         return response    
     
-    if not request_ok and error_type=='warning':
+    if not columns_ok and error_type=='warning':
         warning_description = error_description
         warning = True
+     
+    _id = observation['admission_id']
     
-    #categories_ok, error_description = check_categorical_values(observation)
-    #if not categories_ok:
-    #    response = {"observation_id": _id, 'error': error_description}
-    #    r = Request(request=obs_dict, response=response, endpoint='predict', status='error')
-    #    r.save()
-    #    return response
+    column_types_ok, error_description = check_column_types(observation)
+    if not column_types_ok:
+        response = {"admission_id": _id, 'error': error_description}
+        r = Request(request=observation, response=response, endpoint='predict', status='error')
+        r.save()
+        return response
     
     obs = pd.DataFrame([observation], columns=columns).astype(dtypes)
     probability = pipeline.predict_proba(obs)[0, 1]
-    prediction = int(pipeline.predict(obs) [0])
-    response = {'id':_id, 'probability': probability, 'prediction':prediction}
-    
-    p = Prediction(admission_id=_id, probability=probability, prediction=str(prediction), observation=observation)
+    prediction = get_model_prediction(pipeline.predict(obs)[0])
+    response = {'readmitted':prediction}
+    p = Prediction(admission_id=_id, probability=probability, prediction=prediction, observation=observation)
     
     try:
         p.save()
-        r = Request(request=obs_dict, response=response, endpoint='predict', status='success')
+        r = Request(request=observation, response=response, endpoint='predict', status='success')
         if warning:
             response['warning'] = warning_description
         r.save()
     except IntegrityError:
-        error_msg = "ERROR: Observation ID: '{}' already exists".format(_id)
+        error_msg = "ERROR: Admission ID: '{}' already exists".format(_id)
         response = {'id':_id, 'error': error_msg}
         db.rollback()
-        r = Request(request = obs_dict, response = response, endpoint = 'predict', status = 'error')
+        r = Request(request = observation, response = response, endpoint = 'predict', status = 'error')
         r.save()
         return response
         
@@ -282,21 +317,22 @@ def predict():
 @app.route('/update', methods=['POST'])
 def update():
     
-    obs_dict = request.get_json()
+    observation = request.get_json()
     try:
-        p = Prediction.get(Prediction.observation_id == obs_dict['admission_id'])
-        p.true_class = obs_dict['true_class']
+        p = Prediction.get(Prediction.admission_id == observation['admission_id'])
+        _id = observation['admission_id']
+        p.true_class = observation['readmitted']
         p.modified_date = datetime.datetime.now()
         p.save()
-        response = model_to_dict(p)
-        r = Request(request=obs_dict, response=response, endpoint='update', status='success')
+        response = {'admission_id':_id, 'actual_readmitted':observation['readmitted'] , "predicted_readmitted":p.prediction }
+        r = Request(request=observation, response=response, endpoint='update', status='success')
         r.save()
         return jsonify(response)
     
     except Prediction.DoesNotExist:
-        error_msg = 'Observation ID: "{}" does not exist'.format(obs_dict['admission_id'])
+        error_msg = 'Observation ID: "{}" does not exist'.format(observation['admission_id'])
         response = {'error': error_msg}
-        r = Request(request=obs_dict, response=response, endpoint='update', status='error')
+        r = Request(request=observation, response=response, endpoint='update', status='error')
         r.save()
         return jsonify(response)
 
